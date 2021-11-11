f3325ebdadf55b5c79177aad1138173bf7c1fbb3
kosuke-17 <tamutamu17@icloud.com>
2021-11-11 11:46:32 +0900

    演習1のview作成

---
+++ src/views/Ex01.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Hello World</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class baseballTeamList extends Vue {}
+</script>
+
+<style></style>
