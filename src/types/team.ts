b35e6636fcd4fa8b5942c04c1ccd27196951860e
kosuke-17 <tamutamu17@icloud.com>
2021-11-11 11:47:45 +0900

    球団クラスの作成

---
+++ src/types/team.ts
@@ -0,0 +1,40 @@
+export class Team {
+  constructor(
+    private _name: string,
+    private _homePlace: string,
+    private _startDate: Date,
+    private _history: string
+  ) {}
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public set name(name: string) {
+    this._name = name;
+  }
+
+  public get homePlace(): string {
+    return this._homePlace;
+  }
+
+  public set homePlace(homePlace: string) {
+    this._homePlace = homePlace;
+  }
+
+  public get startDate(): Date {
+    return this._startDate;
+  }
+
+  public set startDate(startDate: Date) {
+    this._startDate = startDate;
+  }
+
+  public get history(): string {
+    return this._history;
+  }
+
+  public set history(history: string) {
+    this._history = history;
+  }
+}
